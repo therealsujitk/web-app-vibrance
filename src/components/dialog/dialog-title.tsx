b0a256a97e755148cc0c00dfb3bdc9b04a7814b0
Sujit Kumar <me@therealsuji.tk>
2022-06-18 21:25:18 +0530

    Add custom dialog component

---
+++ src/components/dialog/dialog-title.tsx
@@ -0,0 +1,37 @@
+import CloseIcon from '@mui/icons-material/Close';
+import { DialogTitle as MaterialDialogTitle, DialogTitleProps as MaterialDialogTitleProps, IconButton } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import React from 'react';
+
+interface DialogTitleProps extends MaterialDialogTitleProps {
+  /**
+   * If defined, a close button will be displayed
+   */
+  onClose? : () => void;
+}
+
+const StyledDialogTitle = styled(MaterialDialogTitle)<DialogTitleProps>(({ theme, onClose }) => ({
+  alignItems: 'center',
+  color: theme.palette.primary.main,
+  display: 'flex',
+  fontWeight: 'bold',
+  justifyContent: onClose ? 'space-between' : 'center',
+}));
+
+export default class DialogTitle extends React.Component<DialogTitleProps> {
+
+  render() {
+    const { children, ...others } = this.props;
+
+    return (
+      <StyledDialogTitle {...others}>
+        {children}
+        { others.onClose && 
+          <IconButton color="primary" onClick={others.onClose}>
+            <CloseIcon />
+          </IconButton>
+        }
+      </StyledDialogTitle>
+    );
+  }
+}
