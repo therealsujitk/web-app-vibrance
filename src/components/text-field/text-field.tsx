9cc4785a9e4224c0cf9b1d146850668912345e6b
Sujit Kumar <me@therealsuji.tk>
2022-06-18 21:25:27 +0530

    Add custom text field component

---
+++ src/components/text-field/text-field.tsx
@@ -0,0 +1,43 @@
+import { TextField as MaterialTextField, BaseTextFieldProps as MaterialTextFieldProps } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import React from 'react';
+
+interface TextFieldProps extends MaterialTextFieldProps {
+  /**
+   * If `true`, text will be center aligned
+   * @default false
+   */
+  centerAlign? : boolean;
+}
+
+const StyledTextField = styled(MaterialTextField)<TextFieldProps>(({ theme, centerAlign }) => ({
+  '& .MuiOutlinedInput-root': {
+    background: theme.palette.background.paper,
+    '& input': {
+      textAlign: centerAlign ? 'center' : 'left',
+    },
+    '& fieldset': {
+      borderColor: 'transparent',
+      textAlign: 'center',
+      transition: 'border-color 0.3s',
+    },
+    '&:hover fieldset': {
+      borderColor: 'transparent',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: theme.palette.primary.main,
+    }
+  },
+}));
+
+export default class TextField extends React.Component<TextFieldProps> {
+  static defaultProps : TextFieldProps = {
+    centerAlign: false,
+  };
+  
+  render() {    
+    return (
+      <StyledTextField {...this.props}/>
+    );
+  }
+}
