859fb3fb4ede84e00a3acad523faa8c75bf75f46
Sujit Kumar <me@therealsuji.tk>
2022-12-10 15:11:10 +0530

    Display splash screen while fetching session

--- frontend/admin/src/app.tsx
+++ frontend/admin/src/app.tsx
@@ -3,8 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { DarkTheme } from './theme';
 import { ThemeProvider } from '@emotion/react';
 import { AppContext, AppContextInterface } from './contexts/app';
+import Logo from "./assets/images/logo.png";
 import React from 'react';
 import Cookies from 'js-cookie';
+import { CircularProgress, CssBaseline, Typography } from '@mui/material';
+import Network from './utils/network';
 
 interface AppState {
   apiKey?: string;
@@ -25,7 +28,7 @@ export default class App extends React.Component<{}, AppState> {
   }
 
   render() {
-    var appContext: AppContextInterface = {
+    const appContext: AppContextInterface = {
       apiKey: this.state.apiKey,
       username: this.state.username,
       permissions: this.state.permissions,
@@ -49,16 +52,63 @@ export default class App extends React.Component<{}, AppState> {
       }
     };
 
+    const Splash = () => (
+      <div>
+        <CssBaseline />
+        <div style={{ 
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          transform: 'translate(-50%, -50%)',
+          textAlign: 'center',
+          width: '100%'
+        }}>
+          <img style={{ width: 'min(300px, 80%)', padding: '20px' }} src={Logo} />
+          <Typography variant="h6" sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '20px',
+            paddingTop: 0
+          }}>
+            <CircularProgress sx={{ marginRight: 2 }} size={24} />
+            Dashboard is loading...
+          </Typography>
+        </div>
+      </div>
+    );
+
     return (
       <ThemeProvider theme={DarkTheme}>
         <AppContext.Provider value={appContext}>
           <BrowserRouter basename='/admin'>
             <AppContext.Consumer>
-              { ({ apiKey }) => apiKey ? <Home /> : <Login /> }
+              { ({ apiKey, username, setSession }) => {
+                if (!apiKey) {
+                  return <Login />
+                } else if (!username) {
+                  this.getSession(apiKey, setSession);
+                  return <Splash />;
+                } else {
+                  return <Home />
+                }
+              }}
             </AppContext.Consumer>
           </BrowserRouter>
         </AppContext.Provider>
       </ThemeProvider>
     );
+  }
+
+  getSession = async (apiKey: string, setSession: (username: string, permissions: string[]) => void) => {
+    try {
+      const response = await new Network(apiKey).doGet('/api/latest/session');
+      const username = response.session.username;
+      const permissions = response.session.permissions;
+
+      setSession(username, permissions);
+    } catch (err) {
+
+    }
   }
 }
