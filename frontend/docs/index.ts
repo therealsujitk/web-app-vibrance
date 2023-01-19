4c8c4fda5b78aa7f4c6fb3a08adcfe36ce033195
Sujit Kumar <me@therealsuji.tk>
2023-01-19 16:40:58 +0530

    Migrate api docs to react

--- frontend/docs/index.ts
+++ frontend/docs/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 const docsRouter = express.Router();
 
-docsRouter.use(express.static(__dirname + '/node_modules/redoc/bundles'));
-docsRouter.use(express.static(__dirname + '/public'));
+docsRouter.use(express.static(__dirname + '/build'));
+docsRouter.get('/*', (req, res) => {
+  res.status(200).sendFile(__dirname + '/build/index.html');
+});
 
 export default docsRouter;
