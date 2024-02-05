cf4a0070b32a472346cf9968c320f9c9ade4902a
Sujit Kumar <60378235+therealsujitk@users.noreply.github.com>
2024-02-05 13:53:32 +0530

    Move home build files to dist

--- frontend/move-builds.ts
+++ frontend/move-builds.ts
@@ -7,3 +7,7 @@ fs.cpSync('./admin/dist', '../dist/frontend/admin/dist', { recursive: true })
 // Moving docs build files
 fs.rmSync('../dist/frontend/docs/dist', { recursive: true, force: true })
 fs.cpSync('./docs/dist', '../dist/frontend/docs/dist', { recursive: true })
+
+// Moving home build files
+fs.rmSync('../dist/frontend/home/dist', { recursive: true, force: true })
+fs.cpSync('./home/dist', '../dist/frontend/home/dist', { recursive: true })
